ebac1d8d4f364df35dfd2f08b36baa7429f3aec0
anunun <161950475+anunun@users.noreply.github.com>
2025-01-25 18:28:39 +0200

    Add files via upload

---
+++ game.py
@@ -0,0 +1,13 @@
+from direct.showbase.ShowBase import ShowBase
+from mapmanager import Mapmanager
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.land = Mapmanager(self)  # Передаємо об'єкт ShowBase до Mapmanager
+        self.camLens.setFov(90)  # Встановлюємо поле огляду камери
+        # self.land.addBlock((0, 0, 0))
+        self.land.loadLand("land.txt")
+
+game = Game()
+game.run()
